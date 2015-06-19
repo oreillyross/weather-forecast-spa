547bd63c37636d166674ec8b71207ff3ffd62de8
Ross O'Reilly <oreillyross@gmail.com>
2015-06-19 20:42:33 +0200

    app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+
+weatherApp.config(function  ($routeProvider){
+    $routeProvider  
+    
+    .when('/', {
+      templateUrl: 'pages/home.htm',
+      controller: 'homeController'    
+    
+    }) 
+
+})
+
+weatherApp.config(function  ($routeProvider){
+    $routeProvider  
+    
+    .when('/forecast', {
+      templateUrl: 'pages/forecast.htm',
+      controller: 'forecastController'    
+    
+    }) 
+
+})
+
+
+// CONTROLLERS
+
+weatherApp.controller('homeController', ['$scope', function($scope) { 
+  
+}]) ;
+
+weatherApp.controller('forecastController', ['$scope', function($scope) { 
+  
+}]) ;
+
+
+
